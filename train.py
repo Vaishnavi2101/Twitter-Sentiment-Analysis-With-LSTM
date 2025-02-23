6fac4c9f40089b51646f9d41f00eaf22263e107f
Vaishnavi2101 <vaishnavi.yaramati07@gmail.com>
2025-02-23 15:18:34 +0530

    Initial Commit

--- train.py
+++ train.py
@@ -19,7 +19,6 @@ df_train.drop(columns=['ID', 'Company'], inplace=True)
 
 # Function to clean tweets
 def clean_tweet(text):
-    text = re.sub(r'http\S+|www\S+|https\S+', '', text, flags=re.MULTILINE)  # Remove URLs
     text = re.sub(r'\@\w+|\#', '', text)  # Remove mentions and hashtags
     text = re.sub(r'\W', ' ', text)  # Remove special characters
     text = re.sub(r'\d', ' ', text)  # Remove numbers
